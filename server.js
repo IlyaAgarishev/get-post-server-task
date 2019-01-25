e8dcb7bab846f77ecf45aa73582730eed561bbe3
IlyaAgarishev <ilyagarishev@gmail.com>
2019-01-25 14:25:14 +0500

    POST REQUEST DONE

--- server.js
+++ server.js
@@ -19,11 +19,12 @@ require('http')
   .createServer(function(req, res) {
     let pathname = decodeURI(url.parse(req.url).pathname);
     console.log('Method: ', req.method);
-    // let fileName;
+    let fileName;
     switch (req.method) {
       case 'GET':
         if (pathname == '/') {
           sendFile(__dirname + '/public/index.html', res);
+          return;
         } else {
           let fileName = permitFileName(pathname);
           console.log('[Get]: Filename:', fileName, ' pathname: ', pathname);
@@ -35,12 +36,86 @@ require('http')
           sendFile(FILES_FOLDER + fileName, res);
           return;
         }
+      case 'POST':
+        fileName = permitFileName(pathname);
+        console.log('[Post]: FileName: ', fileName, ' pathname: ', pathname);
+        if (!fileName) {
+          errorResponse(STATUS_CODES.UNSUPPORTED_PATH, res);
+          return;
+        }
 
+        writeFile(FILES_FOLDER + fileName, req, res);
+        return;
       default:
-        break;
+        res.statusCode = 502;
+        res.end('Not implemented');
     }
   })
   .listen(3000);
+
+function writeFile(filePath, req, res) {
+  let fileSize = req.headers['content-length'];
+  if (fileSize && fileSize > FILES_SIZE_LIMIT) {
+    errorResponse(STATUS_CODES.FILE_TOO_BIG, res);
+    return;
+  }
+
+  let options = { flags: 'wx', autoClose: true };
+  let writeFileStream = fs.createWriteStream(filePath, options);
+
+  req.pipe(writeFileStream);
+
+  req.on('data', checkDataSize);
+
+  req.on('open', () => {
+    console.log('req open');
+  });
+
+  req.on('close', () => {
+    console.log('req close');
+
+    req.removeListener('data', checkDataSize);
+
+    writeFileStream.destroy();
+
+    fs.unlink(filePath, err => {
+      if (err) console.log(err);
+    });
+  });
+
+  let writtenBytes = 0;
+  function checkDataSize(data) {
+    writtenBytes += data.length;
+    console.log('Data: ', data.length, ' | All: ', writtenBytes);
+
+    if (writtenBytes > FILES_SIZE_LIMIT) {
+      errorResponse(STATUS_CODES.FILE_TOO_BIG, res);
+
+      req.removeListener('data', checkDataSize);
+
+      fs.unlink(filePath, err => {
+        if (err) console.error(err);
+      });
+    }
+  }
+
+  writeFileStream.on('error', err => {
+    handleFileErrors(err, res);
+  });
+
+  writeFileStream.on('finish', () => {
+    res.statusCode = STATUS_CODES.OK;
+    res.end();
+  });
+
+  writeFileStream.on('open', () => {
+    console.log('WriteFile open');
+  });
+
+  writeFileStream.on('close', () => {
+    console.log('WriteFile close');
+  });
+}
 
 function permitFileName(fileName) {
   let PERMITTED_FILES_REGEXP = /^\/[^/]+$/;
